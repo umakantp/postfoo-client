40a411917684b4922c1cddf232b88b0415637236
umakantmanu <umakant@manufactured.com>
2025-01-20 22:29:53 +0530

    Add sentry tracking

---
+++ sentry.client.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+const mode = process.env.NEXT_PUBLIC_MODE
+const enabled = mode !== 'local'
+
+Sentry.init({
+  enabled,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  environment: mode,
+  release: process.env.NEXT_PUBLIC_RELEASE,
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
