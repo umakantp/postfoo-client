fa493cac89f72bba0780eb4cad64dcc504162edb
umakantmanu <patil@umakant.com>
2025-02-15 10:36:28 +0530

    core: update sentry, react query and codegen

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,6 @@ const nextConfig = {
         { key: 'X-Client-Release', value: process.env.NEXT_PUBLIC_RELEASE },
         { key: 'X-Client-Release-At', value: process.env.NEXT_PUBLIC_RELEASE_AT },
         { key: 'X-Client-Mode', value: process.env.NEXT_PUBLIC_MODE },
-
       ],
     }]
   },
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,12 +1,8 @@
 import * as Sentry from '@sentry/nextjs'
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
+  if (process.env.NEXT_RUNTIME === 'nodejs' || process.env.NEXT_RUNTIME === 'edge') {
     await import('../sentry.server.config')
-  }
-
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('../sentry.edge.config')
   }
 }
 
